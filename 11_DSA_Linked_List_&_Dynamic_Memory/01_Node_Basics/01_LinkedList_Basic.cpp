92c53299002a0f7ff7afd5ee4a0bb060a288d7c2
Hino Joshpher S. Ronquillo <ronquillohinojoshpher@gmail.com>
2026-09-25 12:14:30 +0800

    DSA_11/Node_01 new task 1

--- 11_DSA_Linked_List_&_Dynamic_Memory/01_Node_Basics/01_LinkedList_Basic.cpp
+++ 11_DSA_Linked_List_&_Dynamic_Memory/01_Node_Basics/01_LinkedList_Basic.cpp
@@ -2,3 +2,22 @@
 
 using namespace std;
 
+struct Node {
+    int data = 10;
+    Node* next = nullptr;
+};
+
+int main() {
+
+    Node node;
+
+    cout << "Data: " << node.data << "\n";
+
+    if (node.next == nullptr) {
+        cout << "Next: nullptr\n";
+    } else {
+        cout << "Next: points to another node\n";
+    }
+
+    return 0;
+}

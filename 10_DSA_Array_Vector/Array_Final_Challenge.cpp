955bb6a5f752d0b5f2fe69ba42ecba817d2c672c
Hino Joshpher S. Ronquillo <ronquillohinojoshpher@gmail.com>
2026-09-25 10:41:50 +0800

    Fix selection sort comparison counter

--- 10_DSA_Array_Vector/Array_Final_Challenge.cpp
+++ 10_DSA_Array_Vector/Array_Final_Challenge.cpp
@@ -55,6 +55,7 @@ int main() {
     for(int i = 0; i < n - 1; i++) {
         int minIndex = i;
         for(int j = i + 1; j < n; j++) {
+            comparisons++;
             if(num[j] < num[minIndex]) {
                 minIndex = j;
             }
